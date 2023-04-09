161925561cfba5e363a43f29f4ff2fec7554a047
Tung Hor Woon <woonth@gmail.com>
2023-04-09 18:10:52 +0930

    added backend

---
+++ backend/api/index.js
@@ -0,0 +1,11 @@
+module.exports = async function (context, req, data) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+        context.bindings.outputDocument = data[0];
+        context.bindings.outputDocument.count += 1;
+  
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: data[0].count
+    };
+}
